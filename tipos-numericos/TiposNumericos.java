c9bfac2fae6beffddce19c302a127d3f5b9e5c8b
Eyke Mesquita <eyke1313@gmail.com>
2024-05-30 00:17:48 -0300

    Primeiro Commit
    Conhecendo Tipos Numéricos e seu "range" numérico.

---
+++ tipos-numericos/TiposNumericos.java
@@ -0,0 +1,19 @@
+
+public class TiposNumericos {
+	
+	public static void main(String[] args) {
+		Byte numeroByte = 127; // -128 até 127
+		
+		Short numeroShort = -32768; // -32768 até 32767
+		
+		Integer numeroInteger = -2147483648; // -2147483648 até 2147483647 (2^31)
+		
+		Long numeroLong = 1l; // -9,223372037 x 10^18  até 9,223372037 x 10^18 (2^63)
+		
+		Float numerofloat = 1.0f; // -3.40292347E+38 até +3.40292347E+38
+		
+		Double numeroDouble = 1.0; // -1.79769313486231570E+308 até +1.79769313486231570E+308
+
+	}
+
+}
