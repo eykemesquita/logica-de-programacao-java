b3850d8db32c57920529e9aa79b7a803c0c6a4e3
Eyke Mesquita <eyke1313@gmail.com>
2024-05-30 18:36:33 -0300

    Primeiro Commit
    Tipos Primitivos na Linguagem Java.

---
+++ tipos-primitivos/TiposPrimitivos.java
@@ -0,0 +1,23 @@
+
+public class TiposPrimitivos {
+	
+	public static void main(String[] args) {
+		
+		byte variavelByte = 1;
+		
+		short variavelShort = 1;
+		
+		int variavelInteger = 1;
+		
+		long variavelLong = 1L;
+		
+		float variavelFloat = 1.0f;
+		
+		double  variavelDouble = 1.0;
+		
+		char variavelCharacter = 'A';
+		
+		boolean variavelBoolean = true;
+	}
+
+}
