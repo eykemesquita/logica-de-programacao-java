e9472eefca66d97cd4272742b226922a1ef27151
Eyke Mesquita <eyke1313@gmail.com>
2024-06-17 11:59:36 -0300

    Primeiro Commit
    Controller.

---
+++ incluindo-as-paginas-html-no-projeto/AlgaWorksContatos/src/main/java/com/algaworks/ContatosControle.java
@@ -0,0 +1,14 @@
+
+package com.algaworks;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContatosControle {	
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
