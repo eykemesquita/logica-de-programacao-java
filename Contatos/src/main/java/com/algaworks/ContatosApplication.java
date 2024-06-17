bdbea4db1c7e9a69c5496661a73cb2b851360b6a
61344905390 <61344905390@NUTIN-05.senacma.local>
2024-06-17 10:57:13 -0300

    Primeiro Commit

---
+++ Contatos/src/main/java/com/algaworks/ContatosApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatosApplication.class, args);
+	}
+
+}
